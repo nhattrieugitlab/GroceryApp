ae52818a9ffdfac87adaeee92837ad91d24a5f39
nhattrieu <lab.nhattrieu203@gmail.com>
2023-06-09 21:28:10 +0700

    Upload component Card

--- App.tsx
+++ App.tsx
@@ -9,15 +9,15 @@ import React from 'react';
 import {
   SafeAreaView, View,
 } from 'react-native';
-import Card from './Card';
+import Card from './src/components/Card';
 
 function App(): JSX.Element {
 
   return (
     <SafeAreaView style={{ padding: 50 }}>
-      <Card image={require('./purepng1.png')} name='Red Apple' price='4,99' weight='1kg,priceg' />
+      <Card image={require('./src/assert/images/purepng1.png')} name='Red Apple' price='4,99' weight='1kg,priceg' />
       <View style={{ marginTop: 12 }}>
-        <Card image={require('./PikPng1.png')} name='Orginal Banana' price='5,99' weight='1kg,priceg' />
+        <Card image={require('./src/assert/images/PikPng1.png')} name='Orginal Banana' price='5,99' weight='1kg,priceg' />
       </View>
     </SafeAreaView>
   );
