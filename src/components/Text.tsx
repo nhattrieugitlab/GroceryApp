c83f9dacd896db55b9e6ff06d953a5b5d5cd36c5
HNPQui <huynhqui670@gmail.com>
2023-06-09 22:11:16 +0700

    Create Text.Tsx by Qui

--- src/components/Text.tsx
+++ src/components/Text.tsx
@@ -1,10 +1,10 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { StyleSheet, View } from 'react-native'
 import React from 'react'
 
 const Text = () => {
   return (
     <View>
-      <Text>Text</Text>
+      
     </View>
   )
 }
