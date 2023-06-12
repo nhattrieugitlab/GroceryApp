9f7128319d38386bc8a5bbf1f72795d392b50560
vanthanhhoang <hoangvanthanhdev@gmail.com>
2023-06-12 21:15:16 +0700

    sua path

--- App.tsx
+++ App.tsx
@@ -6,8 +6,7 @@
  */
 
 import React from 'react';
-import ExploreScreen from './src/components/screens/ExploreScreen';
-import ShopScreen from './src/components/screens/ShopScreen';
+import ExploreScreen from './src/views/ExploreScreen';
 
 function App(): JSX.Element {
 //cmt cua thanh
