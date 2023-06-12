eec8e580179f38921977d6dc96ecd53abfd422ce
vanthanhhoang <hoangvanthanhdev@gmail.com>
2023-06-13 00:30:57 +0700

    merge

--- App.tsx
+++ App.tsx
@@ -6,24 +6,9 @@
  */
 
 import React from 'react';
-<<<<<<< HEAD
-import ExploreScreen from './src/components/screens/ExploreScreen';
-import ShopScreen from './src/components/screens/ShopScreen';
-import SignPass from './src/components/screens/SignPass';
-
-function App(): JSX.Element {
-//cmt cua thanh
-  return (
-    <ExploreScreen />
-  
-  );
-=======
 import AppNavigator from './src/routes/AppNavigator';
-
 function App(): JSX.Element {
   //cmt cua thanh
   return <AppNavigator />;
->>>>>>> bed808d41e3c649ff25421570bd5b79dd701901f
 }
-
 export default App;
