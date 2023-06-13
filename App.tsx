7a6c6060aed86d039360ed442ebc07807d633279
vanthanhhoang <hoangvanthanhdev@gmail.com>
2023-06-13 12:47:58 +0700

    sua app

--- App.tsx
+++ App.tsx
@@ -5,17 +5,6 @@ import WelComeScreen from './src/views/WelcomeScreen';
 import AuthStackNavigator from './src/routes/AuthNavigator';
 function App(): JSX.Element {
   //cmt cua thanh
-<<<<<<< HEAD
   return <AppNavigator />;
-=======
-
-  return <AppNavigator />;
- 
-  
-
-
-  return <EnterPasswordScreen />;
-
->>>>>>> 3dfe087288fe226f0834599c729608066d8cdf8e
 }
 export default App;
